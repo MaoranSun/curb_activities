e608a2d2f6fa2fef265eb0e12ba2716f5c70c698
MaoranSun <31110005+MaoranSun@users.noreply.github.com>
2020-11-03 14:42:15 -0500

    Create install.sh

---
+++ yolo/pi/install.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+sudo apt-get install python3-opencv
+sudo apt-get install libcblas-dev
+sudo apt-get install libhdf5-dev
+sudo apt-get install libhdf5-serial-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libjasper-dev
+sudo apt-get install libqtgui4
+sudo apt-get install libqt4-test
+pip3 install -r requirements.txt
+make
