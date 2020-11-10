92a06d76be1386c19b55d345165b3e11ef3b4cf8
MaoranSun <31110005+MaoranSun@users.noreply.github.com>
2020-11-10 15:09:13 -0500

    Create start.sh

---
+++ yolo/pi/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+python3 darknet_curb_video.py \
+	--out_filename=data/testvideo/curb.avi \
+	--out_db=data/result/curb \
+  --topic=RATP/Entries \
+	--weights=weights/yolov4-tiny-curb_best.weights \
+	--dont_show \
+	--ext_output \
+	--config_file=cfg/yolov4-tiny-curb.cfg \
+	--data_file=data/curb.data
