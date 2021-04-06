fb82a06f15bc54b097f33fc86ed67e44ddfd6f82
MaoranSun <31110005+MaoranSun@users.noreply.github.com>
2021-04-06 12:43:13 -0400

    Update start.sh

--- yolo/pi/start.sh
+++ yolo/pi/start.sh
@@ -3,6 +3,7 @@ python3 darknet_curb_video.py \
 	--out_img=data/testimg \
 	--out_db=data/result/curb \
 	--vflip \
+	--hflip \
   	--topic=RATP/Entries \
 	--weights=weights/yolov4-tiny-curb_best.weights \
 	--dont_show \
